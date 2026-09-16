a4dbad764dba2a5cb43daa52f5e0b657e3c8e738
IlyaBerezin <berezinie@yandex.ru>
2026-09-16 20:59:10 +0100

    first commit this project

---
+++ src/main/java/org/example/constants/Data.java
@@ -0,0 +1,12 @@
+package org.example.constants;
+
+public class Data {
+    public static final String DATE = "18.09.2026";
+    public static final String COMMENT_ORDER = "Оставить у двери";
+
+    public static final String BLACK_COLOR = "black";
+    public static final String GREY_COLOR = "grey";
+
+    public static final String ONE_DAY = "сутки";
+    public static final String TWO_DAYS = "двое суток";
+}
---
+++ src/test/java/org/example/OrderCreationTest.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.example.pages.MainPage;
+import org.example.pages.OrderCreationPage;
+import static org.example.constants.Data.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.junit.jupiter.api.AfterEach;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderCreationTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+    // Тест верхней кнопки Заказать
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {
+            "upperBottom|Березин|Илья|Москва, ул. Тверская, 1|Маяковская|79991234009",
+            "downBottom|Василий|Петров|Москва, ул. Пушкина, 11|Сокольники|79997674321"
+    })
+    public void orderScooterTest(String buttonType, String name, String surname,
+                                 String address, String metro, String phone) {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open();
+        mainPage.clickApproval();
+
+        // Выбор кнопки «Заказать» в зависимости от параметра
+        if ("upperBottom".equals(buttonType)) {
+            mainPage.clickOrderUp();
+        } else {
+            mainPage.clickOrderDown();
+        }
+
+        OrderCreationPage orderPage = new OrderCreationPage(driver);
+
+        // Шаг 1
+        orderPage.fillFirstStep(name, surname, address, metro, phone);
+
+        // Шаг 2
+        orderPage.fillSecondStep(DATE, ONE_DAY, BLACK_COLOR, COMMENT_ORDER);
+
+        // Подтверждение
+        orderPage.clickConfirmButton();
+        boolean confirmed = orderPage.isOrderConfirmed();
+
+        assertTrue(confirmed, "Заказ не оформлен через кнопку: " + buttonType);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
